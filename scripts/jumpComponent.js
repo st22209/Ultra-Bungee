093a22ab6b72e8653f3fc870abb9709db4b1ae15
FusionSid <siddheshadsv@icloud.com>
2023-06-30 23:01:19 +1200

    style: format with prettier

--- scripts/jumpComponent.js
+++ scripts/jumpComponent.js
@@ -2,32 +2,34 @@
  * @param {{jumpName: string, description: string, jumpHeight: number, adultPrice: number, kidsPrice: number, imageURLS: string[]}} data The data for the component
  */
 function fillPageContent(data) {
-  $("#content").load("components/jump/jump.html", () => {
-    $("#jump-name").text(
-      `Feel the rush of our ultimate ${data.jumpName} jump.`
-    );
-    $("#jump-height").text(data.jumpHeight);
-    $("#jump-description").text(data.description);
-    $("#adult-price").text(`$${data.adultPrice}`);
-    $("#kids-price").text(`$${data.kidsPrice}`);
-    $("#hero").css({
-      "background-image": `url("assets/${data.imageURLS[0]}")`,
+    $("#content").load("components/jump/jump.html", () => {
+        $("#jump-name").text(
+            `Feel the rush of our ultimate ${data.jumpName} jump.`
+        );
+        $("#jump-height").text(data.jumpHeight);
+        $("#jump-description").text(data.description);
+        $("#adult-price").text(`$${data.adultPrice}`);
+        $("#kids-price").text(`$${data.kidsPrice}`);
+        $("#hero").css({
+            "background-image": `url("assets/${data.imageURLS[0]}")`,
+        });
+        $("#small-gallery-jump-name").text(
+            `${
+                data.jumpName.charAt(0).toUpperCase() + data.jumpName.slice(1)
+            } Jump`
+        );
+        $("#small-gallery-jump-title").text(
+            `Get an idea of the thrill of Ultra Bungee's ${
+                data.jumpName.charAt(0).toUpperCase() + data.jumpName.slice(1)
+            } jump`
+        );
+        data.imageURLS.map((url) =>
+            $("#small-gallery-images").append(
+                `<a data-fancybox="gallery" href="assets/${url}"> <img src="assets/${url.replace(
+                    ".jpg",
+                    "_thumb.jpg"
+                )}"></a>`
+            )
+        );
     });
-    $("#small-gallery-jump-name").text(
-      `${data.jumpName.charAt(0).toUpperCase() + data.jumpName.slice(1)} Jump`
-    );
-    $("#small-gallery-jump-title").text(
-      `Get an idea of the thrill of Ultra Bungee's ${
-        data.jumpName.charAt(0).toUpperCase() + data.jumpName.slice(1)
-      } jump`
-    );
-    data.imageURLS.map((url) =>
-      $("#small-gallery-images").append(
-        `<a data-fancybox="gallery" href="assets/${url}"> <img src="assets/${url.replace(
-          ".jpg",
-          "_thumb.jpg"
-        )}"></a>`
-      )
-    );
-  });
 }
--- scripts/loadComponents.js
+++ scripts/loadComponents.js
@@ -1,5 +1,10 @@
 $("body").prepend('<nav id="navbar"></nav>');
 $("body").append('<footer id="footer"></footer>');
 
-$("#navbar").load("components/navbar/navbar.html");
+$("#navbar").load("components/navbar/navbar.html", () => {
+    if (window.location.href.includes("st22209.github.io")) {
+        $("#homeurl").attr("href", "https://st22209.github.io/Ultra-Bungee/")
+    }
+});
+
 $("#footer").load("components/footer/footer.html");
